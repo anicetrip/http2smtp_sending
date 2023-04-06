0b250d80d812deba27ceb15f83e147d13dddfe8e
xdryk <yin1232017@outlook.com>
2023-04-07 00:55:23 +0100

    3425

--- src/setup.rs
+++ src/setup.rs
@@ -5,7 +5,7 @@ use crate::service::email_front::email_api;
 
 pub async fn http_front() -> std::io::Result<()> {
     HttpServer::new(|| App::new().wrap(TracingLogger::default()).service(email_api))
-        .bind(("0.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
